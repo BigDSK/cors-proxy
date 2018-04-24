664e5d221519a001bdcd5529a87cf9b07e0f65a9
Dennis Keller <dennis@gozio.me>
2018-04-23 22:57:57 -0700

    Async/await

--- handler.js
+++ handler.js
@@ -1,6 +1,5 @@
-'use strict';
-
-const request = require('request');
+const request = require('request-promise');
+const URL = require('url');
 
 /**
  * Use this command to launch the handler from console:
@@ -11,54 +10,56 @@ const request = require('request');
  *
  * http://localhost:3000/?url=https://github.com
  */
-module.exports.corsProxy = (event, context, callback) => {
-    let params = event.queryStringParameters;
+module.exports.corsProxy = async (event, context, callback) => {
+  let params = event.queryStringParameters;
 
-    console.log(event);
-    console.log(`Got request with params:`, params);
+  console.log(event);
+  console.log(`Got request with params:`, params);
 
-    if (!params.url) {
-        const errorResponse = {
-            statusCode: 400,
-            body: 'Unable get url from \'url\' query parameter'
-        };
+  if (!params.url) {
+    const errorResponse = {
+      statusCode: 400,
+      body: 'Unable get url from \'url\' query parameter',
+    };
 
-        callback(null, errorResponse);
+    callback(null, errorResponse);
 
-        return;
-    }
+    return;
+  }
 
-    return new Promise((resolve, reject) => {
-        request({
-            url: params.url,
-            method: event.httpMethod,
-            timeout: 20000
-        }, (err, originalResponse) => {
-            if (err) {
-                console.log(`Got error`, err);
+  const url = decodeURIComponent(params.url);
 
-                callback(err);
+  const parsed = URL.parse(url);
 
-                reject(err);
+  const headers = {
+    'host': parsed.host,
+  };
 
-                return;
-            }
-
-            console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
-
-            const proxyResponse = {
-                statusCode: originalResponse.statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    "content-type": originalResponse.headers['content-type']
-                },
-                body: originalResponse.body
-            };
-
-            callback(null, proxyResponse);
-
-            resolve(proxyResponse);
-        });
+  try {
+    const response = await request({
+      url,
+      method: event.httpMethod,
+      timeout: 115000,
+      headers,
+      resolveWithFullResponse: true,
     });
+
+    console.log(`Got response from ${params.url} ---> {statusCode: ${response.statusCode}}`);
+
+    const proxyResponse = {
+      statusCode: response.statusCode,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+        "content-type": response.headers['content-type'],
+      },
+      body: response.body,
+    };
+
+    callback(null, proxyResponse);
+  } catch (err) {
+    console.log(`Got error`, err);
+
+    callback(err);
+  }
 };
